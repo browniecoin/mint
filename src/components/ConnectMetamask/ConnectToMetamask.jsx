ec32b05c5a8232b0fa369669c27ee3ec57f3580c
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-09 22:08:55 -0700

    udpate

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -19,7 +19,9 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
         <h1 className="display-5 p-4">
           World Domination One Brownie at a Time!
         </h1>
-        <canvas id="hashPowerChart" ></canvas>
+        <p className="p-5" >
+          <canvas id="hashPowerChart" ></canvas>
+        </p>
         <p >
         Welcome to the Home of Brownie Coin, where we've fused the whimsy of brownie points with the world of cryptocurrencies. Our digital assets ecosystem is refreshingly different, with the essence of our meme coin being essentially worthless, but its community, nostalgia, and art are priceless. Join us for a lighthearted crypto adventure where fun takes precedence over financial pressure, and where we celebrate the essence of brownie points in the digital age. Come on board and explore a cryptocurrency community like no other!
         </p>
