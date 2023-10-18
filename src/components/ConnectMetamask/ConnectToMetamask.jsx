7259deae18dec2ebfd38fd09a8559610748b7a42
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-18 13:08:19 -0700

    update

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -26,7 +26,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
         <p className="mt-3">
         Welcome to the Home of Brownie Coin, where we've fused the whimsy of brownie points with the world of cryptocurrencies. Our digital assets ecosystem is refreshingly different, with the essence of our meme coin being essentially worthless, but its community, nostalgia, and art are priceless. Join us for a lighthearted crypto adventure where fun takes precedence over financial pressure, and where we celebrate the essence of brownie points in the digital age. Come on board and explore a cryptocurrency community like no other!
         </p>
-        <p className="p-5 border">
+        <p className="p-5">
           <canvas id="coinSupplyChart" ></canvas>
         </p>
         <p >
