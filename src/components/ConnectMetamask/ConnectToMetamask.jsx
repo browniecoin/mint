c23a480fe82bbef17fd12f0a4b4417d2e1c2b813
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-18 01:01:43 -0700

    update

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -17,7 +17,7 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
     <div className="container" >
       <div className="jumbotron custom-jumbotron">
         <h1 className="display-5 p-4">
-          World Domination One Brownie at a Time!
+          Brownie Coin
         </h1>
         <p >
         Welcome to the Home of Brownie Coin, where we've fused the whimsy of brownie points with the world of cryptocurrencies. Our digital assets ecosystem is refreshingly different, with the essence of our meme coin being essentially worthless, but its community, nostalgia, and art are priceless. Join us for a lighthearted crypto adventure where fun takes precedence over financial pressure, and where we celebrate the essence of brownie points in the digital age. Come on board and explore a cryptocurrency community like no other!
